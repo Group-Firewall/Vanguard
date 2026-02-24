f112745c02861581b554344804b0312ec93c9e49
Absolom Orianga <absolomjr100@gmail.com>
2026-02-24 17:30:50 +0300

    Feat: Fixed Actions toggle on the Alerts Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-
-
 import Dashboard from './components/Dashboard'
 import './styles/index.css'
 
